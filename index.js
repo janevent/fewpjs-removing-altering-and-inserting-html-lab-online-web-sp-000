b9dcee3be31a70d83115838913a3ef6ac849b4f2
Jane Venter <janeanneventer@gmail.com>
2019-10-11 00:39:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
 let m = document.getElementById("#main");
 m.remove();
-let newHeader = document.createElement("h1 #victory");
+let newHeader = document.createElement("h1");
 newHeader.innerHTML = `${YOUR-NAME} is the champion`
