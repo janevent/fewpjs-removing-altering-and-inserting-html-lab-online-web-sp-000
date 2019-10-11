372dfb02f2fd068a4eb38524c75ef65e5986d804
Jane Venter <janeanneventer@gmail.com>
2019-10-11 01:00:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ m.remove();
 var newHeader = document.createElement("h1");
 newHeader.id = "victory";
 newHeader.innerHTML = `${YOUR-NAME} is the champion`;
+var newContent = document.createTextNode("Hi there and greetings!"); 
+  // add the text node to the newly created div
+  newDiv.appendChild(newContent); 
 
 document.body.appendChild(newHeader);
